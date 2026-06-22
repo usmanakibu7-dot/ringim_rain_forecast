888c80fb8932e5b150aadbdb7eb75c4b71527b32
usmanakibu7-dot <usmanakibu7@gmail.com>
2026-06-22 13:59:21 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import joblib
+import streamlit as st
+
+model=joblib.load('project_model.pkl')
+
+temperature=st.number_input('Enter todays temberature: ')
+humidity=st.number_input('enter humidity: ')
+wind_speed=st.number_input('enter wind speed: ')
+
+if st.button('submit'):
+    dt=pd.DataFrame({
+    'temperature':[temperature],
+    'humidity':[humidity],
+    'wind_speed':[wind_speed]
+    })
+    result=model.predict(dt)
+        if result>=0.5:
+            st.success('There will be rain today')
+        else:
+            st.success('There will be no rain')
